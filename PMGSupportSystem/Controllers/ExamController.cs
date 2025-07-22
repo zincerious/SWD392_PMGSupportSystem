2aba6954a6665fcff6c04df550a20be3aa146907
danh-pham235 <phamdanh0305@gmail.com>
2025-07-22 22:26:20 +0700

    test

--- PMGSupportSystem/Controllers/ExamController.cs
+++ PMGSupportSystem/Controllers/ExamController.cs
@@ -84,7 +84,7 @@ namespace PMGSupportSystem.Controllers
             return Ok(result);
         }
 
-        [Authorize(Roles = "Examiner,Lecturer")]
+        [Authorize(Roles = "Examiner")]
         [HttpGet("exams-examiner")]
         public async Task<ActionResult<IEnumerable<Exam>>> GetExamsByExaminerAsync()
         {
